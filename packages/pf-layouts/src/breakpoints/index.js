357dfc2a623486ddf8061d714124b257f50d05dd
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-03-14 15:34:56 -0500

    Rework tsconfig

--- packages/pf-layouts/src/breakpoints/index.js
+++
@@ -1,7 +0,0 @@
-export const breakpoints = {
-    mobile: { maxWidth: 384 },
-    tablet: { minWidth: 384, maxWidth: 768 },
-    laptop: { minWidth: 768, maxWidth: 1024 },
-    desktop: { minWidth: 1024, maxWidth: 1440 },
-    tv: { minWidth: 1440 }
-};
