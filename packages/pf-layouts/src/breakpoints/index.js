1bc04a6e052e4957b9f82a43af71487235f2b858
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-03-14 15:25:56 -0500

    exclude dist folder

---
+++ packages/pf-layouts/src/breakpoints/index.js
@@ -0,0 +1,7 @@
+export const breakpoints = {
+    mobile: { maxWidth: 384 },
+    tablet: { minWidth: 384, maxWidth: 768 },
+    laptop: { minWidth: 768, maxWidth: 1024 },
+    desktop: { minWidth: 1024, maxWidth: 1440 },
+    tv: { minWidth: 1440 }
+};
