1a2a33de791ed4f6019a7af9bd918247b76b3c7e
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-03-21 20:36:31 -0500

    Add rank property to heading

--- packages/pf-core/src/00-sections/pf-heading.ts
+++ packages/pf-core/src/00-sections/pf-heading.ts
@@ -1,10 +1,31 @@
-import { LitElement, html, customElement } from "lit-element";
+import { LitElement, html, property, customElement } from "lit-element";
 
 @customElement("pf-heading")
 export class PrefabHeading extends LitElement {
+  @property({ type: Number }) rank: number = 2;
+
   render() {
-    return html`
-      <h2><slot></slot></h2>
-    `;
+    switch (this.rank) {
+      case 1:
+        return html`<h1><slot></slot></h1>`;
+        break;
+      case 2:
+        return html`<h2><slot></slot></h2>`;
+        break;
+      case 3:
+        return html`<h3><slot></slot></h3>`;
+        break;
+      case 4:
+        return html`<h4><slot></slot></h4>`;
+        break;
+      case 5:
+        return html`<h5><slot></slot></h5>`;
+        break;
+      case 6:
+        return html`<h6><slot></slot></h6>`;
+        break;
+      default:
+        return html`<h2><slot></slot></h2>`;
+    }
   }
 }
