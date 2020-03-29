3f8a9f0ad75de45c7fc115ebeb487d7363c4deb5
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-03-29 18:58:37 -0500

    Add fallbacks

--- packages/pf-icons/src/index.ts
+++ packages/pf-icons/src/index.ts
@@ -5,12 +5,12 @@ import icons from "./svg.json";
 
 @customElement("pf-icon")
 export class PrefabIcon extends LitElement {
-  @proprty({ type: String }) type: string = "noop"
+  @property({ type: String }) type: string = "noop"
 
   render() {
     return html`
-      <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-        ${unsafeSVG(icons[this.type])}
+      <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" width="30px">
+        ${unsafeSVG((<any>icons)[this.type] || (<any>icons)["noop"])}
       </svg>
     `;
   }
