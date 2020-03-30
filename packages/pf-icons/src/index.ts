edb5a96d793ce72a61aca238f755c08cb9a49b5d
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-03-29 19:30:01 -0500

    Set properties

--- packages/pf-icons/src/index.ts
+++ packages/pf-icons/src/index.ts
@@ -5,11 +5,19 @@ import icons from "./svg.json";
 
 @customElement("pf-icon")
 export class PrefabIcon extends LitElement {
-  @property({ type: String }) type: string = "notdef"
+  @property({ type: String }) type: string = "notdef";
+  @property({ type: String }) size: string = "30px";
+  @property({ type: String }) color: string = "#000";
 
   render() {
     return html`
-      <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" width="30px">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        width="${this.size}"
+        fill="${this.color}"
+        stroke="${this.color}"
+      >
         ${unsafeSVG((<any>icons)[this.type] || (<any>icons)["notdef"])}
       </svg>
     `;
