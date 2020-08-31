903513af163c65922fc1c91d664125f86600b131
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-08-30 19:36:13 -0500

    save MVP hello

--- packages/pf-hello/src/index.ts
+++ packages/pf-hello/src/index.ts
@@ -3,8 +3,6 @@ import { LitElement, html, customElement } from "lit-element";
 @customElement("pf-hello")
 export class PrefabHello extends LitElement {
   render() {
-    return html`
-      <span>Hello, world!</span>
-    `;
+    return html`<span>Hello, world!</span>`;
   }
 }
