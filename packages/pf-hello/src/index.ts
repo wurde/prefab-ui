3e86657bea3aea90c479390a1e13b9689c94c742
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-09-12 00:26:15 -0500

    rm hello

--- packages/pf-hello/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { LitElement, html, customElement } from "lit-element";
-
-@customElement("pf-hello")
-export class PrefabHello extends LitElement {
-  render() {
-    return html`<span>Hello, world!</span>`;
-  }
-}
