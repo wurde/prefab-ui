cf11b4ff884bd0fab92a2caf9f5ac2e8b53f3ef9
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-09-11 23:31:55 -0500

    rm examples

--- stories/examples/Hello.stories.js
+++
@@ -1,9 +0,0 @@
-import { PrefabHello } from "../../packages/pf-hello/src/index";
-
-export default {
-  title: "examples/Hello",
-};
-
-const Template = (args) => new PrefabHello();
-
-export const Primary = Template.bind({});
