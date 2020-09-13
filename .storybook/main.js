9f3dd0c8ec725e27496c2ded1f814f35b4f3f704
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-09-13 01:13:16 -0500

    Add @storybook/addon-a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,11 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: [
+    "@storybook/addon-a11y",
+    "@storybook/addon-essentials",
+    "@storybook/addon-links"
+  ],
+};
